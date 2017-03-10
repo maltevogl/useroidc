7c4149c33aeb9d2bd19d75f0be5707703837e260
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-03-10 12:25:48 +0100

    typo and whitelisting

--- lib/Controller/AuthController.php
+++ lib/Controller/AuthController.php
@@ -61,45 +61,56 @@ class AuthController extends Controller {
         $this->oidc->authenticate();
 
 				$this->session['oidc_access_token'] = $this->oidc->getAccessToken();
-				$this->log->debug('Got access token:' . $this->session['oidc_access_token']);
+				$this->log->debug('Got access token:' . $this->session['oidc_access_token'],['app' => $this->appName]);
 				$this->session['oidc_id_token'] = $this->oidc->getIdToken();
-				$this->log->debug('Got id token:' . $this->session['oidc_id_token']);
+				$this->log->debug('Got id token:' . $this->session['oidc_id_token'],['app' => $this->appName]);
 
 				$this->session['oidc_sub_claim'] = $this->oidc->getSubClaim();
-				$this->log->debug('Got sub claim:' . $this->session['oidc_sub_claim']);
+				$this->log->debug('Got sub claim:' . $this->session['oidc_sub_claim'],['app' => $this->appName]);
 				$sub_array = explode('  ',trim($this->session['oidc_sub_claim']));
         $user_sub = reset($sub_array);
         $connector = end($sub_array);
-				$this->log->debug('Got user from sub:' . $user_sub);
-				$this->log->debug('Got connector from sub:' . $connector);
+				$this->log->debug('Got user from sub:' . $user_sub,['app' => $this->appName]);
+				$this->log->debug('Got connector from sub:' . $connector,['app' => $this->appName]);
 
 				if (strcmp($connector, 'github') == 0) {
 					$this->session['oidc_name_claim'] = $this->oidc->getNameClaim();
-					$this->log->debug('Got name claim:' . $this->session['oidc_name_claim']);
+					$this->log->debug('Got name claim:' . $this->session['oidc_name_claim'],['app' => $this->appName]);
 					$this->session['oidc_email_claim'] = $this->oidc->getEmailClaim();
-					$this->log->debug('Got email claim:' . $this->session['oidc_email_claim']);
+					$this->log->debug('Got email claim:' . $this->session['oidc_email_claim'],['app' => $this->appName]);
 
-					$name_nowhspc = $this->preg_whspc($this->session['oidc_name_claim']);
+					$name_nowhspc = strtolower($this->preg_whspc($this->session['oidc_name_claim']));
 
 					$user_id = implode('_', array($name_nowhspc,$connector));
 					$email = $this->session['oidc_email_claim'];
 					$name = $this->session['oidc_name_claim'];
 				} elseif (strcmp($connector, 'mitre') == 0) {
-					$name_nowhspc = $this->preg_whspc($user_sub);
+					$name_nowhspc = strtolower($this->preg_whspc($user_sub));
 
 					$user_id = implode('_',array($name_nowhspc,$connector));
-					$email = 'dummy@mail.org';
-					$name =  'Dummy MacName';
+					$email = '';
+					$name =  '';
 				} else {
-					$this->log->debug('Got sub from unknown connector');
+					$this->log->debug('Got sub from unknown connector. Login not allowed.',['app' => $this->appName]);
+					return new RedirectResponse('/');
 				}
-        #$email = $this->oidc->requestUserInfo('email');
-        #$name = $this->oidc->requestUserInfo('name');
-        #$user_id = $provider . '__' . $this->oidc->requestUserInfo('sub');
 
         $user = $this->usermanager->get($user_id);
+
         if(!$user) {
-            $this->log->debug(implode(' ',array('Got unknown user:',$user_id,'Please add to db. Admin rights requiered!')));
+            $this->log->debug(implode(' ',array('Got unknown user:',$user_id,'from connector',$connector)),['app' => $this->appName]);
+						$whitelist = file('apps/useroidc/whitelist.txt',FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+						$this->log->debug('Whitelist: '. implode(',',$whitelist),['app' => $this->appName]);
+						if( in_array($user_id,$whitelist) )
+						{
+							$this->log->debug($user_id.' is whitelisted. Will add to db.',['app' => $this->appName]);
+							$user = $this->createUser($user_id, $name, $email);
+							}
+						else
+						{
+							$this->log->debug($user_id.' is not whitelisted, aborting.',['app' => $this->appName]);
+							return new RedirectResponse('/');
+							}
         }
         if(!$user) {
             return new RedirectResponse('/');
