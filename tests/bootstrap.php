ffde4e8f9747e7b80310b7d27df824e04cfc5bda
Malte Vogl <mvogl@mpiwg-berlin.mpg.de>
2017-06-07 14:19:18 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,5 @@
  * @copyright Sigmund Augdal 2016
  */
 
-require_once __DIR__ . '/../../../tests/bootstrap.php';
+require_once __DIR__ . '/../tests/bootstrap.php';
 require_once __DIR__ . '/../appinfo/autoload.php';
